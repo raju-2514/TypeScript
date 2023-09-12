4457262c72c624b58d41dec7e13301b5c62e2545
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2023-09-12 13:22:36 +0600

    Add files via upload
    function and dataType

---
+++ index.ts
@@ -0,0 +1,12 @@
+let userName="Raju Biswas";
+console.log(userName);
+let age=14;
+console.log(age);
+
+function addnumber(num1,num3)
+{
+    return num1+num3;
+}
+console.log(addnumber(14,5));
+console.log(addnumber(14,"54"));
+
