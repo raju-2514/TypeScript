a713ef91c129a1090dcbaaa31927425542b55add
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2023-09-12 21:55:07 +0600

    Add files via upload

---
+++ dataType.js
@@ -0,0 +1,10 @@
+//data type 
+var idNumber;
+var firstName;
+var lastName;
+var isActive;
+idNumber = 12;
+firstName = "Raju";
+lastName = "Biswas";
+isActive = true;
+console.log('ID', { idNumber: idNumber }, firstName, lastName, isActive);
